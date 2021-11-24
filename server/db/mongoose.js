868972e322aec1ffda078075d7a820ad7f7ffcce
Arwinder Singh <arwindersinghh@gmail.com>
2021-11-24 00:09:23 -0500

    env variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+//using dotenv to set mongodb url as env variable
+require('dotenv').config()
 
-mongoose.connect("mongodb+srv://arvy:x7zplh9m@cluster0.unyc6.mongodb.net/simple-mern-app?retryWrites=true&w=majority")
+
+mongoose.connect(process.env.MONGODB_URL)
 .then(result => console.log("We are connected to mongoose"))
 .catch(err => console.log(err))
 
